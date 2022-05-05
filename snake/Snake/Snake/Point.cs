7321c73da9d0134634a583fcefa5ee543666fd51
Eugene Baranovskii <eugene.bar.d17@yandex.ru>
2022-05-05 22:24:10 +0300

    feat: add new class Point

---
+++ snake/Snake/Snake/Point.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class Point
+    {
+        public void DrawSymbol()
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.WriteLine(Symbol);
+        }
+
+        public Point(int x, int y, char symbol) 
+        {
+            X = x;
+            Y = y;
+            Symbol = symbol;
+        }
+
+        private int X { get; set; }
+        private int Y { get; set; }
+        private char Symbol { get; set; }
+    }
+}
--- snake/Snake/Snake/Program.cs
+++ snake/Snake/Snake/Program.cs
@@ -6,7 +6,25 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Let's start!");
+            const int size = 40;
+
+            for (int i = 1; i < size * 2; i += 2)
+            {
+                if (i == 1 || i == size * 2 - 1)
+                {
+                    for (int j = 1; j <= size; j++)
+                    {
+                        new Point(i, j, '*').DrawSymbol();
+                    }
+                }
+                else
+                {
+                    new Point(i, 1, '*').DrawSymbol();
+                    new Point(i, size, '*').DrawSymbol();
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
